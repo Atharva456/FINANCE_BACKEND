b387efb582ec73e2908a8c16b9b4304d795181bd
Atharva456 <atharva.sakhare12@gmail.com>
2022-09-01 19:56:58 +0530

    first commit

---
+++ src/main/java/com/finance/Service/AdminLoginService.java
@@ -0,0 +1,9 @@
+package com.finance.Service;
+
+import com.finance.Model.AdminLogin;
+
+public interface AdminLoginService {
+	public AdminLogin findAdmin(String aname);
+	public boolean addAdmin(AdminLogin admin);
+
+}
---
+++ src/main/java/com/finance/Service/AdminLoginServiceImpl.java
@@ -0,0 +1,28 @@
+package com.finance.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.finance.Model.AdminLogin;
+import com.finance.Repository.AdminLoginRepository;
+
+@Service
+@Transactional
+public class AdminLoginServiceImpl implements AdminLoginService {
+     
+	@Autowired
+	AdminLoginRepository adminloginrepo;
+     
+	@Override
+	public AdminLogin findAdmin(String aname) {
+		
+	return adminloginrepo.findUserByAname(aname);
+	}
+	
+	@Override
+	public boolean addAdmin(AdminLogin admin) {
+		adminloginrepo.save(admin);
+		return true;
+	}
+}
